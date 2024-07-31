a55b9333184eb8d1c2feded247515b29d3291357
gabrielgrillo <gabrielgrillo_4@hotmail.com>
2024-07-31 19:31:00 +0200

    commit

--- app/paridispari.js
+++ app/paridispari.js
@@ -6,9 +6,13 @@
 // Dichiariamo chi ha vinto.
 console.log('pari e dispari')
 
-// let evenOdd = prompt('Scegli pari o dispari')
+//ASSEGNAZIONE DI ELEMENTO DEL DOM
+let msgElement = document.getElementById("msg")
+
+//RICHIESTA INSERIMENTO CAMPI DALL'UTENTE
+let evenOdd = prompt('Scegli pari o dispari').toLowerCase()
 let numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'))
-// console.log(evenOdd, numeroUtente)
+console.log(evenOdd)
 
 //FUNZIONE NUMERO CASUALE
 function numeroCasuale(e) {
@@ -20,11 +24,34 @@ let somma = numeroUtente + numPc
 
 // console.log(numeroUtente)
 // console.log(numPc)
-// console.log(somma)
+
 
 //FUNZIONE CONTROLLO SE PARI
 function isEven(e) {
+    if (somma % 2 == 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+//ASSEGNAZIONE TRUE O FALSE ALLA VARIABILE pari
+let pari = isEven(somma)
+console.log(somma, pari)
+
+//CONDIZIONE DI VERIFICA DOPPIA TRA SELEZIONE PARI/DISPARI E VALORE RITORNATO DALLA FUNZIONE isEven
+if (evenOdd == 'pari' && pari == true) {
+    console.log(`hai vinto`)
+    msgElement.innerHTML += `La somma tra ${numeroUtente} e ${numPc} è ${somma}, Hai vinto`
+}
+else if (evenOdd == "dispari" && pari == false) {
+    console.log(`hai vinto`)
+    msgElement.innerHTML += `La somma tra ${numeroUtente} e ${numPc} è ${somma}, Hai vinto`
 
 }
+else {
+    console.log(`hai perso`)
+    msgElement.innerHTML += `La somma tra ${numeroUtente} e ${numPc} è ${somma}, Hai perso`
 
+}
 
